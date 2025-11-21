4f875eae1471d8c24ad155f95cceda4094a50b2c
namunsoo <namunsoo@naver.com>
2025-11-21 14:01:37 +0900

    수정

--- src/pages/Carrer.js
+++ src/pages/Carrer.js
@@ -11,18 +11,37 @@ function History() {
                         <div class="subheading text-primary mb-3">C#, Blazor, MSSQL, GraphQL, Kendo UI, Anychart</div>
                         <div className="subheading">신규 비즈니스 확장 및 외부 시스템 연동</div>
                         <p>
-                            [주요 역할]<br />
-                            외부 특허 정보 API (Wisdomain)를 연동하여 특허 빅데이터 분석<br />
-                            토스 결제 시스템 활용한 우수기업인증 시스템 구축<br />
-                            신정원 비재무 기반 기업 분석 리포트 개발
+                            [외부 특허 정보 API (Wisdomain)를 연동하여 특허 빅데이터 분석]<br />
+                            - Wisdomain API를 연동하여 특허 데이터 수집 정규화 파이프라인 구현<br />
+                            - 수집된 특허 데이터를 분석 및 시각화 페이지 구현<br />
+                            <br />
+                            [토스 결제 시스템 활용한 우수기업인증 시스템 구축]<br />
+                            - 우수기업 구매 프로세스 설계 및 외부 결제 연동<br />
+                            - 결제 실패/취소 케이스 처리 및 상태관리<br />
+                            <br />
+                            [신정원 비재무 기반 기업 분석 리포트 개발]<br />
+                            - 외부 데이터(비재무 지표) 수집 후 정규화<br />
+                            - 수집된 특허 데이터 시각화<br />
+                            - 리포트 생성 자동화 개발
                         </p>
                         <div className="subheading">기술평가 시스템 기능 고도화 및 품질 향상</div>
                         <p>
-                            [주요 역할]<br />
-                            개인정보보호를 위한 마스킹 기능 구현<br />
-                            평가 프로세스 로직 최적화<br />
-                            담당자/일정 일괄배정 기능 개발<br />
-                            보고서 품질 개선
+                            [개인정보보호를 위한 마스킹 기능 구현]<br />
+                            - 정규식을 사용한 핸드폰 번호 등 개인정보 마스킹 구현<br />
+                            - 마스킹 해제 기록 수집<br />
+                            <br />
+                            [평가 프로세스 로직 최적화]<br />
+                            - 비효율적인 조회 로직 개선<br />
+                            - 코드 가독성 및 유지보수성 향상<br />
+                            <br />
+                            [담당자/일정 일괄배정 기능 개발]<br />
+                            - 요구사항 수집<br />
+                            - 일괄배정 알고리즘 설계 및 구현<br />
+                            - 관리자용 페이지 구현<br />
+                            <br />
+                            [보고서 품질 개선]<br />
+                            - 보고서 가독성 개선<br />
+                            - 보고서 디자인 개선
                         </p>
                     </div>
                     <div className="flex-shrink-0"><span className="text-primary">2024.06 ~ 재직중</span></div>
@@ -31,22 +50,34 @@ function History() {
                     <div className="flex-grow-1">
                         <h3 className="mb-0">네오플러스</h3>
                         <div class="subheading text-primary mb-3">C#, ASP.NET, MSSQL, Kendo UI, Bootstrap, MS Graph api, Azure AI, EWS, AD</div>
-                        <div className="subheading">사내 ASP.NET Core 신규 서비스 개발</div>
+                        <div className="subheading">ASP.NET Core 신규 서비스</div>
                         <p>
                             [주요 역할]<br />
-                            게시글 AI 요약, AI 자동 답변, 게시판 공지 기타 UI 수정
+                            -  Azure AI 를 사용해서 게시글 요약, 질문 답변<br />
+                            - 공지 및 게시판 페이지 작업<br />
+                            - 기타 UI 작
                         </p>
-                        <div className="subheading">Neo 포탈 리팩토링 및 추가기능 개발</div>
+                        <div className="subheading">Neo 포탈 유지보수</div>
                         <p>
                             [주요 역할]<br />
-                            M365 메일 및 사내 AD 계정 비밀번호 변경 기능<br />
-                            경력관리 기능 분석 및 Neo 포탈로 이전<br />
+                            - 회사에서 사요하는 마이크로 소프트 서비스 기능 통합 (MS Graph api 와 MS EWS 기능 통합)<br />
+                            - 마미크로 소프트 업무용 협업 플랫폼 관련 서비스 추가 (MS Teams 서비스)<br />
+                            - 사내 AD 계정 연동<br />
+                            - 오류 분석 및 수정
                         </p>
-                        <div className="subheading">NEO365 기능개선 및 유지보수</div>
+                        <div className="subheading">사내 인트라넷 Neo 포탈로 이전</div>
                         <p>
                             [주요 역할]<br />
-                            NEO365용 그룹, 팀 생성 및 아카이빙 윈도우 서비스 개발<br />
-                            M365 팀, 그룹 생성 및 관리자 기능 개발<br />
+                            - 사내 인트라넷 기능 분석<br />
+                            - 경력관리 시스템 이전<br />
+                            - 전자결제 시스템 이전
+                        </p>
+                        <div className="subheading">NEO365 기능 개발</div>
+                        <p>
+                            [주요 역할]<br />
+                            - 마이크로 소프트 업무용 협업 플랫폼 관련 서비스를 활용 (MS Teams 서비스) 한 팀, 그룹 생성<br />
+                            - 팀, 그룹 관련 관리자 기능 개발<br />
+                            - 생성된 팀, 그룹 데이터 보관(아카이빙)을 윈도우 백그라운드 프로그램 개발(윈도우 서비스)
                         </p>
                     </div>
                     <div className="flex-shrink-0"><span className="text-primary">2022.11 ~ 2024.06</span></div>
